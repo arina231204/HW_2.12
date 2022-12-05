9f759d8c4cbe4cbaae809c7e4728f2b7fb3ff67e
arina231204 <arinaten23@gmail.com>
2022-12-05 15:19:09 +0600

    first commit

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('greetings/', views.greetings, name='greetings'),
+    path('', views.list_item, name='list_item'),
+    path('shop/<int:id>/', views.detail_item, name='detail'),
     ]
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Item, Purchase
 
 def greetings(request):
     return HttpResponse('Добро пожаловать в наш магазин!')
 
+def list_item(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'list_item.html',context)
 
+def detail_item(request, id):
+    item = get_object_or_404(Item, id=id)
+    context = {
+        'item': item
+    }
+    return render(request, 'detail_item.html', context)
 
